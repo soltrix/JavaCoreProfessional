f49f23c930f43ff1fe908bc9b2526877c3383302
dehrom <illigrium@gmail.com>
2018-07-10 17:22:48 +0300

    upd

--- JavaCoreProfessional_Solodkov_homework2/src/main/java/ru/solodkov/javacoreprofessional/App.java
+++ JavaCoreProfessional_Solodkov_homework2/src/main/java/ru/solodkov/javacoreprofessional/App.java
@@ -2,10 +2,20 @@ package ru.solodkov.javacoreprofessional;
 
 import org.sqlite.JDBC;
 
+import java.sql.*;
+
 public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(JDBC.PREFIX + DB_PATH);
+            Statement stmt = conn.createStatement();
+
+            java.sql.DriverManager.registerDriver( new Driver());
+        } catch (SQLException e) {
+            throw new RuntimeException( "Can't register driver!" );
+        }
     }
 }
